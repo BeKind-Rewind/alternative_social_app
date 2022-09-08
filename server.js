79222b231227e7b861184dd7bbcbf1c480726183
Amanda Perry <challenge641@gmail.com>
2022-09-08 15:52:40 -0500

    added aws s3 bucket for photo upload and sending to server.

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const session = require('express-session');
 // set up Handlebars.js as app's template engine of choice
 const exphbs = require('express-handlebars');
 
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const { uploadFile, getFileStream } = require('./s3')
+app.get('/images/:key', (req, res) => {
+  console.log(req.params)
+  const key = req.params.key
+  const readStream = getFileStream(key)
+
+  readStream.pipe(res)
+})
+
+app.post('/images', upload.single('image'), async (req, res) => {
+  const file = req.file
+  console.log(file)
+  // future: apply filter or resize
+  const result = await uploadFile(file)
+  await unlinkFile(file.path)
+  console.log(result)
+  const description = req.body.description
+  res.send({imagePath: `/images/${result.Key}`})
+})
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
